a74c7a7b7ea3f71ba41c22f61e4f3d1906708864
inbrewj-udc <jason@urbandatacollective.com>
2022-06-24 12:49:39 +0100

    install common first

--- install_upstream_dependencies.sh
+++ install_upstream_dependencies.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
+rm -rf common || true && \
+rm -rf rest-utils || true && \
+rm -rf schema-registry || true && \
+rm -rf kafka-connect-storage-common || true
+
 echo "------ INSTALLING UPSTREAM DEPENDENCIES FOR kafka-connect-storage-cloud ------"
 
 # [confluentinc/kafka](https://github.com/confluentinc/kafka)
 git clone https://github.com/confluentinc/kafka.git && cd kafka && git checkout tags/v7.2.0-108-ccs -b v7.2.0-108-ccs && ./gradlewAll -x test publishToMavenLocal
-cd -
-# [confluentinc/rest-utils](https://github.com/confluentinc/rest-utils)
-git clone https://github.com/confluentinc/rest-utils.git && cd rest-utils && git checkout 49f3b66f67f58b4e1c0ddd0a0d642baccec8a122 && mvn clean install -DskipTests
-cd -
-# [confluentinc/schema-registry](https://github.com/confluentinc/schema-registry)
-git clone https://github.com/confluentinc/schema-registry.git && cd schema-registry && git checkout 49f3b66f67f58b4e1c0ddd0a0d642baccec8a122 && mvn clean install -DskipTests
-cd -
+
 # [confluentinc/common](https://github.com/confluentinc/common)
 git clone https://github.com/confluentinc/common.git && cd common && git checkout tags/v7.2.0-949 -b v7.2.0-949 && mvn clean install -DskipTests
 cd -
+
+# [confluentinc/rest-utils](https://github.com/confluentinc/rest-utils)
+git clone https://github.com/confluentinc/rest-utils.git && cd rest-utils && git checkout 49f3b66f67f58b4e1c0ddd0a0d642baccec8a122 && mvn clean install -DskipTests
+cd -
+
+# [confluentinc/schema-registry](https://github.com/confluentinc/schema-registry)
+git clone https://github.com/confluentinc/schema-registry.git && cd schema-registry && git checkout 49f3b66f67f58b4e1c0ddd0a0d642baccec8a122 && mvn clean install -DskipTests
+cd -
+
 # [confluentinc/kafka-connect-storage-common](https://github.com/confluentinc/kafka-connect-storage-common)
 git clone https://github.com/confluentinc/kafka-connect-storage-common.git && cd kafka-connect-storage-common && git checkout tags/v11.0.4 -b 11.0.4 && mvn -U clean install -DskipTests -pl \!:kafka-connect-storage-hive
 cd -
