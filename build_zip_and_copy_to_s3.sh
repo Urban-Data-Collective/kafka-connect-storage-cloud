fc1ca92f5855566e58c633f0d473c54affcf6e65
inbrewj-udc <jason@urbandatacollective.com>
2022-06-29 16:13:44 +0100

    UDX-1216 adds a manual build_zip_and_copy_to_s3.sh since I'm fairly sure we've run out of GitHub actions minutes...

---
+++ build_zip_and_copy_to_s3.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PATH_TO_INFRA=$1
+OUTPUT_ZIP_NAME="confluentinc-kafka-connect-s3-UdxStreamPartitioner.zip"
+S3_PATH="s3://udx-dev-msk-connectors/$OUTPUT_ZIP_NAME"
+
+mvn clean package -DskipTests && \
+echo "Copying ZIP archived build to S3 to path: $S3_PATH" && \
+aws s3 cp kafka-connect-s3/target/components/packages/confluentinc-kafka-connect-s3-10.0.8.zip s3://udx-dev-msk-connectors/$OUTPUT_ZIP_NAME
